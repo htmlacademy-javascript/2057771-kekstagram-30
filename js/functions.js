7a921f65795d11e31e99830b772eb799d9ecf581
Вета <vetarara@yandex.ru>
2023-10-27 03:48:56 +0400

    вторая функция (палиндром)

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,15 @@ if (result) {
 } else {
   console.log('Fail!');
 }
+
+// Функция для проверки, является ли строка палиндромом:
+
+function checkPalindrome(text) {
+  text = text.replaceAll(" ", "").toLowerCase();
+  const reversedText = text.split('').reverse().join('');
+  return text === reversedText;
+}
+
+console.log(checkPalindrome("А роза упала на лапу Азора")); // true
+console.log(checkPalindrome("Лёша на полке клопа нашёл ")); // true
+console.log(checkPalindrome("Привет, мир!")); // false
