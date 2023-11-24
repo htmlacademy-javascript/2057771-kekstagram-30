7df34aa04912eac9137ab5abd4dfb1867c4ef05d
Вета <vetarara@yandex.ru>
2023-11-24 18:06:17 +0400

    добавлены массивы, удалена функция (sentences)

--- js/main.js
+++ js/main.js
@@ -11,6 +11,72 @@
   } вот, как должен выглядеть сгенерированный объект. всего их должно быть 25
   Каждый объект массива — описание фотографии, опубликованной пользователем?
 } */
+
+const DESCRIPTIONS = [
+  'Пляж',
+  'Указатель',
+  'Море',
+  'Девушка',
+  'Хехе',
+  'Машина',
+  'Клубничка',
+  'Морс',
+  'Пока-пока!',
+  'Полочка на колёсиках',
+  'Чистый песок',
+  'Белая ауди',
+  'Закуска с рыбой',
+  'Не предлагайте мне суши, если они не похожи на это!',
+  'Воу',
+  'Путь',
+  'Концерт хора',
+  'Carry on my wayword soon',
+  'Ночь',
+  'пальмы!',
+  'Боул',
+  'Закат',
+  'Краб',
+  'Эминем?',
+  'Опасная ситуация',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Елизавета',
+  'Михаил',
+  'Жанна',
+  'Артём',
+  'Настя',
+  'Тимофей',
+  'Соня',
+  'Юра',
+  'София',
+  'Яна',
+  'Василиса',
+  'Света',
+  'Таисия',
+  'Андрей',
+  'Полина',
+  'Августин',
+  'Бенисио дель Торо',
+  'Дин Винчестер',
+  'Мэттью Беллами',
+  'Тося Чайкина',
+  'Jay-Jay Johanson',
+  'Рената Литвинова',
+  'Ксения',
+  'Нино',
+  'Али',
+];
+
 const postsCount = 25;
 
 const getRandomInteger = (a, b) => {
@@ -20,21 +86,6 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-function getRandomSentences(text, numSentences) {
-  const sentences = text.split(/[\.\?!]/);
-  sentences = sentences.filter(function(sentence) {
-    return sentence.trim() !== '';
-  });
-  const randomSentences = [];
-  for (const i = 0; i < numSentences; i++) {
-    const randomIndex = Math.floor(Math.random() * sentences.length);
-    randomSentences.push(sentences[randomIndex]);
-  }
-  return randomSentences;
-}
-
-const text = 'Всё отлично! В целом всё неплохо. Но не всё. Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально. Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше. Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-
 const createPost = () => {
   const randomUserId = getRandomInteger(1, 25); // Это число от 1 до 25. Идентификаторы не должны повторяться.
   const randomUrl = 'photos/' + getRandomInteger(1, 25) + '.jpg';
@@ -42,7 +93,7 @@ const createPost = () => {
   const description = 'Тут может быть просто повторяющийся текст?';
   const randomLikes = getRandomInteger(1, 25);
   //вряд ли это должно быть тут const comments = [commentId, avatar, message, name];
-  const randomCommentId = getRandomInteger(1, ?); //ЛЮБОЕ число
+  const randomCommentId = getRandomInteger(1, 50); //ЛЮБОЕ число
   const randomAvatar = 'img/avatar-' + getRandomInteger(1, 6) + '.svg';
   const randomMessage = getRandomSentences(text, 2);
   const randomName = getRandomInteger(0, NAMES.length - 1);
